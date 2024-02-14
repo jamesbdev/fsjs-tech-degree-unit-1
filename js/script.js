727a9df018e923297928d0843258db18d4e7e2dd
James Bremner <j.bremner@ymail.com>
2024-02-14 10:39:14 +0000

    add quotes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,39 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: "I know starting careers in troubled times is a challenge, but it is also a privilege. Because it's moments like these that force us to try harder, dig deeper and to discover gifts we never knew we had. To find the greatness that lies within each of us. So don't ever shy away from that endeavor. Don't stop adding to your body of work. I can promise that you will be the better for that continued effort as will be this nation that we all love.",
+    source: "Barack Obama",
+    year: "2009",
+    citation: "Arizona State University Commencement Address, 2009",
+},
+{
+  quote: "You will not find your passion in things and you will not find your passion in money. The more things and the more money you have, the more you will look around and use that as the metric and there will be someone with more.",
+  source: "Randy Pausch",
+  year: "2008",
+  citation: "Carnegie Mellon Commencement Speech, 2008",
+}, 
+{
+  quote: "Life is a foreign language; all men mispronounce it.",
+  source: "Christopher Morley",
+  year: "1890-1956",
+  citation: "",
+},
+ {
+  quote: "When you cannot get a compliment any other way pay yourself one.",
+  source: "Mark Twain",
+  year: "1835-1910",
+  citation: "",
+ },
+  {
+   quote: "Prediction is very difficult, especially about the future.",
+   source: "Niels Bohr",
+   year: "1885-1962",
+   citation: "",
+  }
+];
+
 
 
 
